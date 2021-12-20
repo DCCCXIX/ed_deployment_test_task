edffb7ad0858abe554bced59bf2d4f584b4dac18
DCCCXIX <dethcorddemid@gmail.com>
2021-12-21 01:56:26 +0300

    added logging, moved mh instantiation

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python
+import datetime
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler("app.log", "w", "utf-8")
+logger.addHandler(handler)
 import os
 
 import waitress
-from flask import Flask, request
+from flask import Flask, Response, request
 
 import model_handler
 
 app = Flask(__name__)
+mh = model_handler.Model_Handler()
 
 
-@app.route("/emotion-detection", methods=["GET", "POST"])
-def predict_form_post():
-    text = request.args.get("text")
-    return model_handler.mh.predict(text)
+@app.route("/emotion-detection", methods=["POST"])
+def predict_post():
+    try:
+        text = request.get_json()["text"]
+        logging.info(f"[{datetime.datetime.now()}]Request: {text}")
+        result = mh.predict(text)
+        logging.info(f"[{datetime.datetime.now()}]Result: {result}")
+        return mh.predict(text)
+    except Exception as e:
+        logging.exception(f"[{datetime.datetime.now()}]Bad request")
+        return Response("Bad request", status=400)
 
 
 if __name__ == "__main__":
