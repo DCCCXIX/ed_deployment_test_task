67fc2f4f2381c2cdba2d18c3b9d4b204b23556f1
DCCCXIX <dethcorddemid@gmail.com>
2021-12-21 01:56:33 +0300

    added tests

---
+++ unittests.py
@@ -0,0 +1,38 @@
+import unittest
+
+from transformers.utils.dummy_pt_objects import DataCollator
+
+import main
+
+
+class ControllerTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = main.app
+        self.app.config["TESTING"] = True
+        self.client = self.app.test_client(self)
+        self.payload = {"text": "a mock text for testing purposes"}
+        self.text = "a mock phrase for testing purposes"
+
+    def test_predict(self):
+        result = main.mh.predict(self.text)
+
+        assert type(result) is dict
+        assert len(result) == main.mh.model.classifier.out_features
+        assert not all(value == 0 for value in result.values())
+
+    def test_predict_post(self):
+        response = self.client.post("/emotion-detection", json=self.payload)
+        response_dict = response.json
+
+        assert response.status_code == 200
+        assert type(response_dict) is dict
+        assert not all(value == 0 for value in response_dict.values())
+
+    def test_predict_post_invalid(self):
+        response = self.client.post("/emotion-detection", data=self.payload)
+
+        assert response.status_code == 400
+
+
+if __name__ == "__main__":
+    unittest.main()
