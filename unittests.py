359ec74fb7e90d7f18170ad4b5826fdaa0b564c1
DCCCXIX <dethcorddemid@gmail.com>
2021-12-22 07:24:04 +0300

    fixed asserts

--- unittests.py
+++ unittests.py
@@ -1,7 +1,5 @@
 import unittest
 
-from transformers.utils.dummy_pt_objects import DataCollator
-
 import main
 
 
@@ -10,29 +8,28 @@ class ControllerTestCase(unittest.TestCase):
         self.app = main.app
         self.app.config["TESTING"] = True
         self.client = self.app.test_client(self)
-        self.payload = {"text": "a mock text for testing purposes"}
-        self.text = "a mock phrase for testing purposes"
+        self.payload = {"text": "Sample text for testing purposes."}
 
     def test_predict(self):
-        result = main.mh.predict(self.text)
+        result = main.mh.predict(self.payload["text"])
 
-        assert type(result) is dict
-        assert len(result) == main.mh.model.classifier.out_features
-        assert not all(value == 0 for value in result.values())
-        assert abs(sum(result.values()) - 1) < 0.000001
+        self.assertIs(type(result), dict)
+        self.assertEqual(len(result), main.mh.model.model.classifier.out_features)
+        self.assertTrue(not all(value == 0 for value in result.values()))
+        self.assertTrue(abs(sum(result.values()) - 1) < 0.000001)
 
     def test_predict_post(self):
         response = self.client.post("/emotion-detection", json=self.payload)
         response_dict = response.json
 
-        assert response.status_code == 200
-        assert type(response_dict) is dict
-        assert not all(value == 0 for value in response_dict.values())
+        self.assertEqual(response.status_code, 200)
+        self.assertIs(type(response_dict), dict)
+        self.assertTrue(not all(value == 0 for value in response_dict.values()))
 
     def test_predict_post_invalid(self):
         response = self.client.post("/emotion-detection", data=self.payload)
 
-        assert response.status_code == 400
+        self.assertEqual(response.status_code, 400)
 
 
 if __name__ == "__main__":
